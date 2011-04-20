fd99b14c69f9f624ae543667543ce52852cfbff8
Jeremy Hubert <jhubert@gmail.com>
2011-04-20 14:20:49 -0700

    Abstract the SQL creation methods

--- sqlite.js
+++ sqlite.js
@@ -9,6 +9,16 @@ function SQLite(cfg) {
     }
   }
 
+  function isNumber(val) {
+    switch (typeof val) {
+    case 'number':
+      return true;
+    case 'string':
+      return /^\d+$/.test(val);
+    case 'object':
+      return false;
+    }
+  }
 
   // Default Handlers
   function nullDataHandler(results) { }
@@ -62,126 +72,129 @@ function SQLite(cfg) {
     );
   }
 
-  return {
-    database: db,
-    createTable: function (name, cols, data, error) { 
-      var query = "CREATE TABLE " + name + "(" + cols + ");";
-      execute(query, null, data, error);
-    },
-    insert: function (table, map, data, error) {
-      var query = "INSERT INTO " + table + " (#k#) VALUES(#v#);", keys = [], values = [], x;
+  function buildConditions(conditions) {
+    var results = [], x;
 
-      for (x in map) {
-        if (map.hasOwnProperty(x)) {
-          keys.push(x);
+    if (typeof conditions === 'string') {
+      results.push(conditions);
+    } else if (typeof conditions === 'number') {
+      results.push("id=" + conditions);
+    } else if (typeof conditions === 'object') {
+      for (x in conditions) {
+        if (conditions.hasOwnProperty(x)) {
+          if (isNumber(x)) {
+            results.push(conditions[x]);
+          } else {
+            if (isNumber(conditions[x])) {
+              results.push(x + '=' + conditions[x]);
+            } else {
+              results.push(x + '="' + conditions[x] + '"');
+            }
+          }
+        }
+      }
+    }
+
+    if (results.length > 0) {
+      results = " WHERE " + results.join(' AND ');
+    } else {
+      results = '';
+    }
+
+    return results;
+  }
+  
+
+  function createTableSQL(name, cols) {
+    var query = "CREATE TABLE " + name + "(" + cols + ");";
+
+    return query;
+  }
+
+  function insertSQL(table, map) {
+    var query = "INSERT INTO " + table + " (#k#) VALUES(#v#);", keys = [], values = [], x;
+
+    for (x in map) {
+      if (map.hasOwnProperty(x)) {
+        keys.push(x);
+        if (isNumber(map[x])) {
+          values.push(map[x]);
+        } else {
           values.push('"' + map[x] + '"');
         }
       }
+    }
 
-      query = query.replace("#k#", keys.join(','));
-      query = query.replace("#v#", values.join(','));
+    query = query.replace("#k#", keys.join(','));
+    query = query.replace("#v#", values.join(','));
 
-      execute(query, null, data, error);
+    return query;
+  }
+
+  function updateSQL(table, map, conditions) {
+    var query = "UPDATE " + table + " SET #k##m#", keys = [], matches = '', x;
+
+    for (x in map) {
+      if (map.hasOwnProperty(x)) {
+        if (isNumber(map[x])) {
+          keys.push(x + '=' + map[x]);
+        } else {
+          keys.push(x + '="' + map[x] + '"');
+        }
+      }
+    }
+
+    matches = buildConditions(conditions);
+
+    query = query.replace("#k#", keys.join(','));
+    query = query.replace("#m#", matches);
+
+    return query;
+  }
+
+  function selectSQL(table, columns, conditions, options) {
+    var query = 'SELECT #col# FROM ' + table + '#cond#;', matches = '';
+
+    if (typeof columns === 'undefined') {
+      columns = '*';
+    } else if (typeof columns === 'object') {
+      columns.join(',');
+    }
+
+    matches = buildConditions(conditions);
+
+    query = query.replace('#col#', columns);
+    query = query.replace('#cond#', matches);
+
+    return query;
+  }
+
+  function destroySQL(table, conditions) {
+    var query = 'DELETE FROM ' + table + '#c#;', matches = '';
+
+    matches = buildConditions(conditions);
+
+    query = query.replace('#c#', matches);
+
+    return query;
+  }
+
+  return {
+    database: db,
+    createTable: function (name, cols, data, error) { 
+      execute(createTableSQL(name, cols), null, data, error);
+    },
+    insert: function (table, map, data, error) {
+      execute(insertSQL(table, map), null, data, error);
     },
     update: function (table, map, conditions, data, error) {
-      var query = "UPDATE " + table + " SET #k##m#", keys = [], values = [], matches = [], x;
-
-      for (x in map) {
-        if (map.hasOwnProperty(x)) {
-          keys.push(x + '=?');
-          values.push(map[x]);
-        }
-      }
-
-      if (typeof conditions === 'string') {
-        matches.push(conditions);
-      } else if (typeof conditions === 'number') {
-        matches.push("id=?");
-        values.push(conditions);
-      } else if (typeof conditions === 'object') {
-        for (x in conditions) {
-          if (conditions.hasOwnProperty(x)) {
-            if (x.match(/^\d+$/)) {
-              matches.push(conditions[x]);
-            } else {
-              matches.push(x + '=?');
-              values.push(conditions[x]);
-            }
-          }
-        }
-      }
-
-      if (matches.length > 0) {
-        matches = " WHERE " + matches.join(' AND ');
-      } else {
-        matches = '';
-      }
-
-      query = query.replace("#k#", keys.join(','));
-      query = query.replace("#m#", matches);
-
-      execute(query, values, data, error);
+      execute(updateSQL(table, map, conditions), null, data, error);
     },
-    select: function (table, columns, conditions, data, error) {
-      var query = 'SELECT #col# FROM ' + table + '#cond#;', matches = [], x;
-
-      if (typeof columns === 'undefined') {
-        columns = '*';
-      } else if (typeof columns === 'object') {
-        columns.join(',');
-      }
-
-      if (typeof conditions === 'string') {
-        matches.push(conditions);
-      } else if (typeof conditions === 'number') {
-        matches.push("id=" + conditions);
-      } else if (typeof conditions === 'object') {
-        for (x in conditions) {
-          if (conditions.hasOwnProperty(x)) {
-            if (x.match(/^\d+$/)) {
-              matches.push(conditions[x]);
-            } else {
-              matches.push(x + '=' + conditions[x]);
-            }
-          }
-        }
-      }
-
-      if (matches.length > 0) {
-        matches = " WHERE " + matches.join(' AND ');
-      }
-
-      query = query.replace('#col#', columns);
-      query = query.replace('#cond#', matches);
-
-      execute(query, null, data, error);
+    select: function (table, columns, conditions, options, data, error) {
+      execute(selectSQL(table, columns, conditions, options), null, data, error);
     },
     destroy: function (table, conditions, data, error) {
-      var query = 'DELETE FROM ' + table + '#c#;', matches = [], x;
-
-      if (typeof conditions === 'string') {
-        matches.push(conditions);
-      } else if (typeof conditions === 'number') {
-        matches.push("id=" + conditions);
-      } else if (typeof conditions === 'object') {
-        for (x in conditions) {
-          if (conditions.hasOwnProperty(x)) {
-            if (x.match(/^\d+$/)) {
-              matches.push(conditions[x]);
-            } else {
-              matches.push(x + '=' + conditions[x]);
-            }
-          }
-        }
-      }
-
-      if (matches.length > 0) {
-        matches = " WHERE " + matches.join(' AND ');
-      }
-
-      query = query.replace('#c#', matches);
-
-      execute(query, null, data, error);
+      execute(destroySQL(table, conditions), null, data, error);
     }
   };
 }
